8da042d697a034dd15a2d8217283c7d23de85db2
Emil Axelsson <emax@chalmers.se>
2015-04-30 21:34:18 +0200

    Slightly more interesting example

--- examples/Simple.hs
+++ examples/Simple.hs
@@ -7,16 +7,17 @@ import Prelude ()
 import Feldspar hiding (getRef, setRef)
 import Feldspar.SimpleVector
 import Feldspar.IO
+import Language.Embedded.Imperative (stdin) -- TODO Export from Feldspar.IO
 
 
 
 kernel :: Data Int32 -> Data Int32
 kernel n = sum $ map (\x -> x*x) (0...n)
 
-prog = do
-    r <- initRef 100
-    i <- getRef r
-    setRef r $ kernel (cap (Range 1 maxBound) i)
-    o <- getRef r
-    printf "%d" o
+prog = while (return true) $ do
+    i <- fget stdin
+    o <- ifE (i > 0)
+        (return $ kernel (cap (Range 1 maxBound) i))
+        (return 0)
+    printf "Sum of squares: %d\n" o
 
