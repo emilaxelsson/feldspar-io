319183c3b0d694d2798dcc2c2d2fd816d991f28f
Emil Axelsson <emax@chalmers.se>
2015-05-06 21:40:53 +0200

    Improved example

--- examples/Simple.hs
+++ examples/Simple.hs
@@ -14,7 +14,9 @@ kernel :: Data Int32 -> Data Int32
 kernel n = sum $ map (\x -> x*x) (0...n)
 
 prog = while (return true) $ do
-    i <- fget stdin
+    i   <- fget stdin
+    end <- feof stdin
+    iff end break (return ())
     o <- ifE (i > 0)
         (return $ kernel (cap (Range 1 maxBound) i))
         (return 0)
